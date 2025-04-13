9abdcc003711664b61286fa60f27ff27bbfb3ef0
Musab74 <Rmcbaxr@gmail.com>
2025-04-14 02:26:39 +0900

    feat: installing mui components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import "../css/app.css";
+import '../css/app.css';  // fixed path
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
-  return <div>HOME</div>;
+  return (
+    <Container sx={{background:"orange"}}>
+      <Stack flexDirection="column">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h4">
+            Create React App on TypeScript with REDUX
+          </Typography>
+        </Box>
+        <RippleBadge badgeContent={4}>
+        <Button variant="contained">Contained</Button>
+        </RippleBadge>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
-
-
-
-
-
