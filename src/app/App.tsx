44a9656d59b3553aaa47e218042164b8f6de79ae
Musab74 <Rmcbaxr@gmail.com>
2025-04-17 19:00:11 +0900

    fix:orginize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import '../css/app.css';
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
 import { HomePage } from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 
 
 function App() {
+ const location = useLocation();
+ console.log(location);
+ 
+
   return (
     <div>
       <nav>
@@ -28,7 +35,7 @@ function App() {
           </li>
         </ul>
       </nav>
-
+   {location.pathname === "/" ? <HomeNavbar/> : <OtherNavbar/>}
       <Switch>
         <Route path="/products">
           <ProductsPage />
@@ -43,6 +50,7 @@ function App() {
           <HomePage />
         </Route>
       </Switch>
+      <Footer/>
     </div>
   )
 }
