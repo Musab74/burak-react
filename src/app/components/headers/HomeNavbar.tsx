ee20edbd4d303d3965952340b0dfcef0b7730e1e
Musab74 <Rmcbaxr@gmail.com>
2025-04-18 18:38:25 +0900

    feat:modify navbar home components

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -47,7 +47,25 @@ export function HomeNavbar() {
 
                     </Stack>
                 </Stack>
-                <Stack>DETAIL</Stack>
+                <Stack className="header-frame">
+                    <Stack className="detail">
+                        <Box className="head-main-text" >
+                            World's Most Delicious Cousine
+                        </Box>
+                        <Box className="wel-txt">The Choice, not just a choice</Box>
+                        <Box className="service-txt">24  hours service</Box>
+                        <Box className="signup" ></Box>
+                        {!AuthMember ? (
+                        <Button variant={"contained"}
+                        className="signup-button"
+                        >
+                            Sign Up</Button>) : null}
+                
+                    </Stack>
+                    <Box className="logo-frame">
+                        <div className="logo-img"></div>
+                    </Box>
+                </Stack>
             </Container>
         </div>
     );
