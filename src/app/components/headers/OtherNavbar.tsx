a3734178806a7876970509d9dcf91d3e5459eb59
Musab74 <Rmcbaxr@gmail.com>
2025-04-18 18:54:42 +0900

    feat:other navbar develop

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,3 +1,54 @@
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function OtherNavbar() {
-    return <div>Other Navbar</div>
-    }
+    const AuthMember = 0;
+    return (
+        <div className="other-navbar">
+            <Container className="navbar-container">
+                <Stack className="menu">
+                    <Box>
+                        <NavLink to="/">
+                            <img className="brand-logo" src="/icons/burak.svg" alt="Brand Logo" />
+                        </NavLink>
+                    </Box>
+                    <Stack className="links">
+                        <Box className="hover-line">
+                            <NavLink to="/">Home</NavLink>
+                        </Box>
+                        <Box className="hover-line">
+                            <NavLink to="/products" activeClassName="underline">Products</NavLink>
+                        </Box>
+                        {AuthMember ? (
+                            <Box className="hover-line">
+                                <NavLink to="/orders" activeClassName="underline">Orders</NavLink>
+                            </Box>
+                        ) : null}
+                        {AuthMember ? (
+                            <Box className="hover-line">
+                                <NavLink to="/member-page" activeClassName="underline">My Page</NavLink>
+                            </Box>
+                        ) : null}
+                        <Box className="hover-line">
+                            <NavLink to="/help" activeClassName="underline">Help</NavLink>
+                        </Box>
+
+                        {/* Login / Avatar */}
+                        {!AuthMember ? (
+                            <Box>
+                                <Button variant="contained" className="login-button">Login</Button>
+                            </Box>
+                        ) : (
+                            <img
+                                className="user-avatar"
+                                src="/icons/default-user.svg"
+                                alt="User Avatar"
+                                aria-haspopup="true"
+                            />
+                        )}
+                    </Stack>
+                </Stack>
+            </Container>
+        </div>
+    );
+}
