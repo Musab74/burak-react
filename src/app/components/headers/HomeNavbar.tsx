a1d1a9dce53528930442bcbe435bb040a0c570f9
Musab74 <Rmcbaxr@gmail.com>
2025-04-20 23:28:52 +0900

    feat: organize home page related sectional components

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
 
 export default function HomeNavbar() {
-    const AuthMember = true;
+    const AuthMember = 0;
     return (
         <div className="home-navbar">
             <Container className="navbar-container">
---
+++ src/app/screens/homePage/NewDishes.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function NewDishes() {
+   return <div>
+        <Container>New Dishes</Container>
+    </div>
+}
