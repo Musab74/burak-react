0c3919c80e4bdafdc22da3bf527506c27f0b7a08
Musab74 <Rmcbaxr@gmail.com>
2025-04-22 22:40:33 +0900

    fix:product page nested routing system

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,5 +1,22 @@
 import { Container } from "@mui/material";
+import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
+import React from "react";
+import ChosenProduct from "./Chosenproduct";
+import Products from "./Products";
 
 export default function ProductsPage() {
-    return <Container>ProductsPage</Container>
+    const products = useRouteMatch();
+
+    return (
+        <div className="products-page">
+            <Switch>
+                <Route path={`${products.path}/:productsId`}>
+                    <ChosenProduct></ChosenProduct>
+                </Route>
+                <Route path={`${products.path}`}>
+                    <Products />
+                </Route>
+            </Switch>
+        </div> >
+    );
 }
