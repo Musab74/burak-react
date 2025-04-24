2a9ccd298d4c89d4269f88a0baa273d1556838c5
Musab74 <Rmcbaxr@gmail.com>
2025-04-24 18:29:32 +0900

    feat:developing chosen product page

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,9 +1,9 @@
 import { Container } from "@mui/material";
 import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
 import React from "react";
-import ChosenProduct from "./Chosenproduct";
 import Products from "./Products";
 import "../../../css/products.css"
+import ChosenProduct from "./ChosenProduct";
 
 export default function ProductsPage() {
     const products = useRouteMatch();
