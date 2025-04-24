58e0f1bddfdbb7fb3a85e846a95ff5f03416d663
Musab74 <Rmcbaxr@gmail.com>
2025-04-25 00:55:32 +0900

    feat:new bugs fixed

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -2,8 +2,8 @@ import { Container } from "@mui/material";
 import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
 import React from "react";
 import Products from "./Products";
-import "../../../css/products.css"
-
+import "../../../css/products.css";
+import ChosenProduct from "./ChosenProduct";
 
 export default function ProductsPage() {
     const products = useRouteMatch();
@@ -11,9 +11,9 @@ export default function ProductsPage() {
     return (
         <div className="products-page">
             <Switch>
-                {/* <Route path={`${products.path}/:productId`}>
+                <Route path={`${products.path}/:productId`}>
                     <ChosenProduct></ChosenProduct>
-                </Route> */}
+                </Route> 
                 <Route path={`${products.path}`}>
                     <Products />
                 </Route>
