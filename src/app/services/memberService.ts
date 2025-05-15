110cb2461d0dcc3c03be75bcc0c8488d029ade83
Musab74 <Rmcbaxr@gmail.com>
2025-05-16 00:15:25 +0900

    feat: modify chosen product page components logic

--- src/app/services/memberService.ts
+++ src/app/services/memberService.ts
@@ -21,7 +21,20 @@ class MemberService {
           throw err;
         }
       }
-      
+    
+    public async getRestaurant(): Promise<Member> {
+        try {
+          const url = this.path + "/member/restaurant";
+          const result = await axios.get(url);
+          console.log("getTopUsers", result);
+          
+          
+          return result.data
+        } catch (err) {
+          console.log("Error, getProduct:", err);
+          throw err;
+        }
+      }  
 }
 
 export default MemberService;
